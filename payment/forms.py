d269e5d10dae6baedf6aee08893ec3cb07729cb2
Nicolae Caldarasan <nicolaecaldarasan1983@gmail.com>
2023-02-09 11:11:02 +0000

    Implement the shipping form

--- payment/forms.py
+++ payment/forms.py
@@ -1,5 +1,16 @@
 from django import forms
 
+from . models import ShippingAddress
+
+class ShippingForm(forms.ModelForm):
+    
+    class Meta:
+
+        mode = ShippingAddress
+
+        fields = ['full_name', 'email', 'address1', 'address2', 'city', 'country', 'postcode']
+
+        exclude = ['user',]
 
 
 
