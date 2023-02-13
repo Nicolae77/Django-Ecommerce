0bde7707eae9e5abfc827567c233d422f2f8d3ab
Nicolae Caldarasan <nicolaecaldarasan1983@gmail.com>
2023-02-13 15:34:52 +0000

    Ajax complete checkout

--- payment/urls.py
+++ payment/urls.py
@@ -7,10 +7,11 @@ urlpatterns = [
 
     path('checkout', views.checkout, name='checkout'),
 
+    path('complete-order', views.complete_order, name='complete-order'),
+
     path('payment-success', views.payment_success, name='payment-success'),
 
     path('payment-failed', views.payment_failed, name='payment-failed'),
-
 
 
 ]
